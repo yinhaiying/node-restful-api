8b60f920efb16f15574c4d5f4386dcf2c9605662
yinhaiying <18840842571@163.com>
2018-11-28 11:22:00 +0800

    连接mongoose服务器

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+  mongoURI:'mongodb://haiyinsitan:aaa123456@ds119210.mlab.com:19210/node-restful-api'
+}
--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const express = require('express');
 
 const app = express();
+const mongoose = require('mongoose');
+const {mongoURI } = require('./config/keys');
+mongoose.connect(mongoURI)
+  .then(() => {
+    console.log('连接成功')
+  });
 
 
 app.get('/',(req,res) => {
